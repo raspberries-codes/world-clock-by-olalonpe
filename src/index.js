876674ea62e65a9f1ddca431c7a392653cced7f4
Lonpsy <oolalonpe@gmail.com>
2025-07-14 17:24:55 +0200

    updated the real time and date of three cities using javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
+function losAngelesElements() {
+  let losAngelesCurrentDate = moment()
+    .tz("America/Los_Angeles")
+    .format("dddd MMMM Do, YYYY ");
+  let losAngelesDate = document.querySelector("#los-Angeles-date");
+  losAngelesDate.innerHTML = losAngelesCurrentDate;
 
+  let losAngelesCurrentTime = moment()
+    .tz("America/Los_Angeles")
+    .format("hh:mm:ss");
+  let los_AngelesTime = document.querySelector("#los-angeles-time");
+  los_AngelesTime.innerHTML = losAngelesCurrentTime;
+
+  let amElement = moment().tz("America/Los_Angeles").format(A);
+  let los_AngelesAm = document.querySelector("#amElement");
+  los_AngelesAm.innerHTML = amElement;
+}
+function parisElement() {
+  let parisCurrentDate = moment()
+    .tz("Europe/Paris")
+    .format("dddd MMMM Do, YYYY ");
+  let parisDate = document.querySelector("#parisDate");
+  parisDate.innerHTML = parisCurrentDate;
+
+  let parisCurrentTime = moment().tz("Europe/paris").format("hh:mm:ss");
+  let parisTime = document.querySelector("#parisTime");
+  parisTime.innerHTML = parisCurrentTime;
+
+  let amElement = moment().tz("Europe/paris").format(A);
+  let parisAm = document.querySelector("#paris-AmElement");
+  parisAm.innerHTML = amElement;
+}
+function nigeriaElement() {
+  let nigeriaCurrentDate = moment()
+    .tz("Africa/Lagos")
+    .format("dddd MMMM Do, YYYY ");
+  let nigeriaDate = document.querySelector("#nigeriaDate");
+  nigeriaDate.innerHTML = nigeriaCurrentDate;
+
+  let nigeriaCurrentTime = moment().tz("Africa/Lagos").format("hh:mm:ss");
+  let nigeriaTime = document.querySelector("#nigeriaTime");
+  nigeriaTime.innerHTML = nigeriaCurrentTime;
+
+  let amElement = moment().tz("Africa/Lagos").format(A);
+  let nigeriaAm = document.querySelector("#nigeria-AmElement");
+  nigeriaAm.innerHTML = amElement;
+}
+
+setInterval(losAngelesElements, 1000);
+setInterval(parisElement, 1000);
+setInterval(nigeriaElement, 1000);
